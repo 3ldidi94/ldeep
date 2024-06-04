07a289c2c842efde345cf8da72ee1e17f94b781e
3ldidi94 <18704024+3ldidi94@users.noreply.github.com>
2024-06-04 10:48:12 +0200

    Update __init__.py

--- ldeep/__init__.py
+++ ldeep/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = open(os.path.join(os.path.dirname(__file__), "VERSION")).read().strip()
+__version__ = open(os.path.join(os.path.dirname(__file__), "../VERSION")).read().strip()
 
 
 def get_version():
